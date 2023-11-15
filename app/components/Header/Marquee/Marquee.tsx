8d15954eeabece9028f9c26a9207c6d063385c75
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-15 14:52:11 +0100

    marquee problem solving

--- app/components/Header/Marquee/Marquee.tsx
+++ app/components/Header/Marquee/Marquee.tsx
@@ -5,12 +5,21 @@ import styles from './marquee.module.css';
 export default function HeaderMarquee() {
   return (
     <Marquee className={styles.marqueeContainer} pauseOnHover={true}>
-      <p>NO MUSIC FOR NOW</p>
-      <div></div>
-      <p>NO MUSIC FOR NOW</p>
-      <div></div>
-      <p>NO MUSIC FOR NOW</p>
-      <div></div>
+      <div className={styles.marqueeElement}>
+        <div className={styles.reddot}></div>
+        <p>NO MUSIC FOR NOW</p>
+        <div className={styles.marqueeSpan}></div>
+      </div>
+      <div className={styles.marqueeElement}>
+        <div className={styles.reddot}></div>
+        <p>NO MUSIC FOR NOW</p>
+        <div className={styles.marqueeSpan}></div>
+      </div>
+      <div className={styles.marqueeElement}>
+        <div className={styles.reddot}></div>
+        <p>NO MUSIC FOR NOW</p>
+        <div className={styles.marqueeSpan}></div>
+      </div>
     </Marquee>
   );
 }
