d4846a050e456787dfdbb6c1960a12bb5810b967
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-15 15:01:26 +0100

    header finished for now with marquee

--- app/components/Header/Marquee/Marquee.tsx
+++ app/components/Header/Marquee/Marquee.tsx
@@ -4,20 +4,25 @@ import styles from './marquee.module.css';
 
 export default function HeaderMarquee() {
   return (
-    <Marquee className={styles.marqueeContainer} pauseOnHover={true}>
+    <Marquee className={styles.marqueeContainer} pauseOnHover={true} speed={20}>
       <div className={styles.marqueeElement}>
         <div className={styles.reddot}></div>
-        <p>NO MUSIC FOR NOW</p>
+        <p>Yung Lean - Iceman</p>
         <div className={styles.marqueeSpan}></div>
       </div>
       <div className={styles.marqueeElement}>
         <div className={styles.reddot}></div>
-        <p>NO MUSIC FOR NOW</p>
+        <p>Playing Now</p>
         <div className={styles.marqueeSpan}></div>
       </div>
       <div className={styles.marqueeElement}>
         <div className={styles.reddot}></div>
-        <p>NO MUSIC FOR NOW</p>
+        <p>Fluffy Radio</p>
+        <div className={styles.marqueeSpan}></div>
+      </div>
+      <div className={styles.marqueeElement}>
+        <div className={styles.reddot}></div>
+        <p>Playing Now</p>
         <div className={styles.marqueeSpan}></div>
       </div>
     </Marquee>
