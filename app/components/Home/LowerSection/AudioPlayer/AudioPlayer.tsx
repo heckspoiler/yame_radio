531d172353b73d854d34dbe7e588d45b1ddf72f2
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-16 17:26:29 +0100

    adding song mockup with audioplayer

--- app/components/Home/LowerSection/AudioPlayer/AudioPlayer.tsx
+++ app/components/Home/LowerSection/AudioPlayer/AudioPlayer.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { allSongs } from '../../../../../songmockup';
 import styles from './audioPlayer.module.css';
 
 const AudioPlayer: React.FC = () => {
--- songmockup.ts
+++ songmockup.ts
@@ -1,4 +1,4 @@
-interface song {
+interface Song {
   id: number;
   title: string;
   artist: string;
@@ -8,3 +8,17 @@ interface song {
   url: string;
   coverArtUrl: string;
 }
+
+export const allSongs: Song[] = [
+  {
+    id: 1,
+    title: 'Iceman',
+    artist: 'Yung Lean',
+    album: 'Stranger',
+    genres: ['Hip Hop', 'Rap', 'Cloudrap', 'Dreamy'],
+    duration: 243,
+    url: 'www.test.com',
+    coverArtUrl:
+      'https://i1.sndcdn.com/artworks-FJX0X0gTK6ONw0ce-RdMjlw-t500x500.jpg',
+  },
+];
