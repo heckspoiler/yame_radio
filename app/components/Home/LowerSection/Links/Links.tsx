eb8d7b362ef948c2275ed42da5166c6c07441917
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-16 09:31:49 +0100

    link section added

--- app/components/Home/LowerSection/Links/Links.tsx
+++ app/components/Home/LowerSection/Links/Links.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import Image from 'next/image';
 import instagramIcon from './icons/instagram.svg';
+import patreonIcon from './icons/patreon.svg';
+import profileIcon from './icons/profile.svg';
+import styles from './links.module.css';
 
 export default function Links() {
   return (
-    <div>
-      {' '}
-      <div>
-        <Image priority src={instagramIcon} alt="Follow us on Twitter" />
-      </div>
+    <div className={styles.linksContainer}>
+      <a href="https://www.google.com" target="_blank">
+        <Image
+          priority
+          src={instagramIcon}
+          alt="Follow us on Twitter"
+          className={styles.image}
+        />
+      </a>
+      <a href="https://www.google.com" target="_blank">
+        <Image
+          priority
+          src={patreonIcon}
+          alt="Follow us on Twitter"
+          className={styles.image}
+        />
+      </a>
+      <a href="https://www.google.com" target="_blank">
+        <Image
+          priority
+          src={profileIcon}
+          alt="Follow us on Twitter"
+          className={styles.image}
+        />
+      </a>
     </div>
   );
 }
