1d99d852a12862683ac965fae67553dd9e72f008
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-20 14:44:22 +0100

    few changes, adding server & database fetching

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,24 @@ import 'normalize.css/normalize.css';
 import { HomePage } from './Home/page';
 import { LatestFind } from './latestfind/page';
 import { News } from './news/News';
+import axios from 'axios';
 
 export default function Home() {
+  const response: any = axios
+    .get('./server/server.js')
+    .then((res) => {
+      const data: any = res.data;
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error: ', error);
+    });
   return (
     <main className={styles.main}>
       <HomePage />
       <LatestFind />
       <News />
+      <h1>{response}</h1>
     </main>
   );
 }
---
+++ server/server.ts
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello world!');
+});
